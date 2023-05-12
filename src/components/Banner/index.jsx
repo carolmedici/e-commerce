bfd1cdfa8b66774f89b1b3d53cc2bed99d8b5f3a
carolmedici <carolina.obstetriz@gmail.com>
2023-05-11 22:29:25 -0300

    ::sparkles: Banner Slider

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,13 +1,34 @@
-import banner from "./image/glasses.jpg"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import React, { Component } from "react";
+import Slider from "react-slick";
+import banner2 from "./image/banner2.jpg"
+import banner3 from "./image/banner3.jpg"
+import banner1 from "./image/glasses.jpg"
 
-const Banner = () =>{
+
+export default class Banner extends Component{
+
+    render (){
+        const settings = {
+
+            dots: true, 
+            infinite: true,
+            spedd: 500,
+            slidesToShow: 1,
+            slidesToScrow: 1
+        };
+
     return(
         <>
-        <img className="w-1/2 block mx-auto mt-5" src={banner} alt="Banner" />
+        <Slider {...settings}>
+        <div> <img className="w-1/2 block mx-auto mt-5" src={banner1} alt="Banner" /></div>
+        <div><img src={banner2} className="w-1/2 block mx-auto mt-5" alt="Sunglass Banner" /></div>
+        <div><img src={banner3} className="w-1/2 block mx-auto mt-5" alt="Sunglass Banner" /></div>
+
+        </Slider>
         </>
 
-    )
+    );
 }
-
-
-export default Banner
+}
