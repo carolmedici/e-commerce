c5d0d067059b6382c887beeaca265ac8ee6e81c5
carolmedici <carolina.obstetriz@gmail.com>
2023-05-14 22:56:16 -0300

    :lipstick: Card

--- src/components/Products/Card/index.jsx
+++ src/components/Products/Card/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Card.css"
 
 const Card = ({ product }) => {
   const handleClickCard = (item) => {
@@ -12,13 +13,16 @@ const Card = ({ product }) => {
       <ul >  
         <li   key={product.tag}>
           <div className="shadow-lg shadow-pink-400/30 w-80 h-100 m-10 rounded-lg">
-              <img className="p-3 rounded-lg w-50 h-30" src={product.photo} alt={product.productName} />
+          <img id="image" className="p-3 rounded-lg w-50 h-30 card-image" src={product.photo} alt={product.productName} />
+
               <h1 className="text-2xl my-2 text-center">{product.productName}</h1>
               <p className="line-through text-base ml-5">$ {product.price}</p>
               <span className="text-xl ml-5 my-1">$ {product.price * 0.9}</span>
               <span className="text-xs ml-2">or 2x $ {product.price / 2}</span>
               <p className="text-xs ml-5 my-2 text-pink-400">Free Shipping</p>
-              <button className="bg-pink-400 hover:bg-pink-800 rounded-lg py-1 px-5 my-1 text-white mx-auto" >BUY</button>
+              <div className="flex justify-center">
+                <button className="bg-pink-400 hover:bg-pink-800 rounded-lg py-1 px-36 my-1 text-white" >BUY</button>
+              </div>
           </div>
         </li>
       </ul>
